ddf16122a7588797d527da342da72011769c0b4c
maaaruf <marufbdonline@gmail.com>
2024-08-24 06:12:36 +0600

    fix namespace indentations

--- Cachy/CacheContext.cs
+++ Cachy/CacheContext.cs
@@ -4,30 +4,29 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Reflection;
 
-namespace Cachy
+namespace Cachy;
+
+public class CacheContext : MemoryDistributedCache
 {
-    public class CacheContext : MemoryDistributedCache
-    {
-        public CacheContext(
+    public CacheContext(
         IOptions<MemoryDistributedCacheOptions> optionsAccessor,
         ILoggerFactory loggerFactory,
         IServiceProvider serviceProvider)
-        : base(optionsAccessor, loggerFactory)
+    : base(optionsAccessor, loggerFactory)
+    {
+        // Initialize CacheSets based on the properties defined
+        foreach (var prop in this.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
         {
-            // Initialize CacheSets based on the properties defined
-            foreach (var prop in this.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            if (prop.PropertyType.IsGenericType &&
+                prop.PropertyType.GetGenericTypeDefinition() == typeof(CacheSet<>))
             {
-                if (prop.PropertyType.IsGenericType &&
-                    prop.PropertyType.GetGenericTypeDefinition() == typeof(CacheSet<>))
-                {
-                    var modelType = prop.PropertyType.GetGenericArguments()[0];
-                    var cacheSet = Activator.CreateInstance(
-                        prop.PropertyType,
-                        serviceProvider.GetService(typeof(IDistributedCache)),
-                        serviceProvider.GetService(typeof(ICacheKeyFactory<>).MakeGenericType(modelType))
-                    );
-                    prop.SetValue(this, cacheSet);
-                }
+                var modelType = prop.PropertyType.GetGenericArguments()[0];
+                var cacheSet = Activator.CreateInstance(
+                    prop.PropertyType,
+                    serviceProvider.GetService(typeof(IDistributedCache)),
+                    serviceProvider.GetService(typeof(ICacheKeyFactory<>).MakeGenericType(modelType))
+                );
+                prop.SetValue(this, cacheSet);
             }
         }
     }
